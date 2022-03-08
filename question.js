bee1e80db8e2421cad04e73ac649b4acc4cc5ffa
Owen <tamia123123@naver.com>
2022-03-08 18:34:37 +0900

    update

--- question.js
+++ question.js
@@ -99,6 +99,9 @@ stopBtn.addEventListener('click' , ()=> {
         clearInterval(timeInterval);
         isPlaying= false;
         answer.readOnly = true;
+        form.addEventListener('submit' , (e)=> {
+            e.stopPropagation();
+        })
         stopBtn.innerHTML = "재시작";
         stopBtn.classList.add('active');
     } else {
